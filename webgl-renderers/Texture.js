38c80847b8146c17d18a77fe17aa16a7c109a65d
Alexander Gugel <alexander.gugel@gmail.com>
2015-07-29 16:33:53 +0200

    refact: Refactor, document Texture
    * Accept GL context constants instead of strings (deprecation notice added;
      backwards-compatible)
    * Properly document all options
    * Make options properties of Texture instance
    * Fix a couple of minor issues and corner cases, e.g. when width/ height is 0

--- webgl-renderers/Texture.js
+++ webgl-renderers/Texture.js
@@ -31,31 +31,73 @@
  * @class Texture
  * @constructor
  *
- * @param {GL} gl GL
- * @param {Object} options Options
- *
- * @return {undefined} undefined
+ * @param {WebGLRenderingContext} gl WebGL Context
+ * @param {Object} [options] Options
+ * @param {Number} [options.width]
+ * @param {Number} [options.height]
+ * @param {Boolean} [options.mipmap]
+ * @param {Number} [options.format=gl.RGBA]
+ * @param {Number} [options.type=gl.UNSIGNED_BYTE]
+ * @param {Number} [options.magFilter=gl.NEAREST]
+ * @param {Number} [options.minFilter=gl.NEAREST]
+ * @param {Number} [options.flipYWebgl=false]
+ * @param {Number} [options.premultiplyAlphaWebgl=false]
+ * @param {Number} [options.wrapS=gl.CLAMP_TO_EDGE]
+ * @param {Number} [options.wrapT=gl.CLAMP_TO_EDGE]
  */
-function Texture(gl, options) {
+function Texture (gl, options) {
     options = options || {};
+
+    this.gl = gl;
     this.id = gl.createTexture();
+
     this.width = options.width || 0;
     this.height = options.height || 0;
     this.mipmap = options.mipmap;
-    this.format = options.format || 'RGBA';
-    this.type = options.type || 'UNSIGNED_BYTE';
-    this.gl = gl;
 
+    // @deprecated Remove when deprecation of string options is complete!
+    if (typeof options.format === 'string')
+        options.format = this.gl[options.format];
+
+    if (typeof options.type === 'string')
+        options.type = this.gl[options.type];
+
+    if (typeof options.magFilter === 'string')
+        options.magFilter = this.gl[options.magFilter];
+
+    if (typeof options.minFilter === 'string')
+        options.minFilter = this.gl[options.minFilter];
+
+    if (typeof options.wrapS === 'string')
+        options.wrapS = this.gl[options.wrapS];
+
+    if (typeof options.wrapT === 'string')
+        options.wrapT = this.gl[options.wrapT];
+    // End of deprecation.
+
+    this.format = options.format != null ? options.format : gl.RGBA;
+    this.type = options.type != null ? options.type : gl.UNSIGNED_BYTE;
+
+    this.magFilter = options.magFilter != null ? options.magFilter : gl.NEAREST;
+    this.minFilter = options.minFilter != null ? options.minFilter : gl.NEAREST;
+
+    this.flipYWebgl = options.flipYWebgl != null ? options.flipYWebgl : false;
+    this.premultiplyAlphaWebgl = options.premultiplyAlphaWebgl != null ? options.premultiplyAlphaWebgl : false;
+
+    this.wrapS = options.wrapS != null ? options.wrapS : gl.CLAMP_TO_EDGE;
+    this.wrapT = options.wrapT != null ? options.wrapT : gl.CLAMP_TO_EDGE;
+
+    // @deprecated No longer bind on construction to avoid side effects!
     this.bind();
 
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, options.flipYWebgl || false);
-    gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, options.premultiplyAlphaWebgl || false);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, this.flipYWebgl);
+    gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, this.premultiplyAlphaWebgl);
 
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl[options.magFilter] || gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl[options.minFilter] || gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, this.magFilter);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, this.minFilter);
 
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl[options.wrapS] || gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl[options.wrapT] || gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, this.wrapS);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, this.wrapT);
 }
 
 /**
@@ -64,7 +106,7 @@ function Texture(gl, options) {
  * @method
  * @return {Object} Current texture instance.
  */
-Texture.prototype.bind = function bind() {
+Texture.prototype.bind = function bind () {
     this.gl.bindTexture(this.gl.TEXTURE_2D, this.id);
     return this;
 };
@@ -75,7 +117,7 @@ Texture.prototype.bind = function bind() {
  * @method
  * @return {Object} Current texture instance.
  */
-Texture.prototype.unbind = function unbind() {
+Texture.prototype.unbind = function unbind () {
     this.gl.bindTexture(this.gl.TEXTURE_2D, null);
     return this;
 };
@@ -86,10 +128,10 @@ Texture.prototype.unbind = function unbind() {
  * @method
  *
  * @param {Image}   img     The image object to upload pixel data from.
- * @return {Object}         Current texture instance.
+ * @return {Texture}        this
  */
-Texture.prototype.setImage = function setImage(img) {
-    this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl[this.format], this.gl[this.format], this.gl[this.type], img);
+Texture.prototype.setImage = function setImage (img) {
+    this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.format, this.format, this.type, img);
     if (this.mipmap) this.gl.generateMipmap(this.gl.TEXTURE_2D);
     return this;
 };
@@ -100,15 +142,15 @@ Texture.prototype.setImage = function setImage(img) {
  * @method
  *
  * @param {Array}   input   Array to be set as data to texture.
- * @return {Object}         Current texture instance.
+ * @return {Texture}        this
  */
-Texture.prototype.setArray = function setArray(input) {
-    this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl[this.format], this.width, this.height, 0, this.gl[this.format], this.gl[this.type], input);
+Texture.prototype.setArray = function setArray (input) {
+    this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.format, this.width, this.height, 0, this.format, this.type, input);
     return this;
 };
 
 /**
- * Dumps the rgb-pixel contents of a texture into an array for debugging purposes
+ * Dumps the RGBA-pixel contents of a texture into an array for debugging purposes
  *
  * @method
  *
@@ -119,20 +161,26 @@ Texture.prototype.setArray = function setArray(input) {
  *
  * @return {Array}          An array of the pixels contained in the snapshot.
  */
-Texture.prototype.readBack = function readBack(x, y, width, height) {
+Texture.prototype.readBack = function readBack (x, y, width, height) {
     var gl = this.gl;
     var pixels;
+
     x = x || 0;
     y = y || 0;
-    width = width || this.width;
-    height = height || this.height;
+
+    width = width != null ? width : this.width;
+    height = height != null ? height : this.height;
+
     var fb = gl.createFramebuffer();
+
     gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.id, 0);
+
     if (gl.checkFramebufferStatus(gl.FRAMEBUFFER) === gl.FRAMEBUFFER_COMPLETE) {
         pixels = new Uint8Array(width * height * 4);
         gl.readPixels(x, y, width, height, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
     }
+
     return pixels;
 };
 
